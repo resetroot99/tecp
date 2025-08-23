5815ccd5a618005e1f40904a642c4bd3fddef2c2
resetroot99 <ali@resetadas.com>
2025-08-22 17:38:42 -0700

    ci: configure GitHub Pages deployment

--- packages/tecp-reference-ui/vite.config.ts
+++ packages/tecp-reference-ui/vite.config.ts
@@ -1,24 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3003,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3002',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/spec': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: '/tecp/',
   build: {
     outDir: 'dist',
-    sourcemap: true,
-  },
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 });
